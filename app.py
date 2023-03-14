0d557bcdffe829e90a386d09f243639af6a4e420
Mihir Pande <127537487+meeheer123@users.noreply.github.com>
2023-03-14 16:25:30 +0000

    made it realistic with real data

--- app.py
+++ app.py
@@ -1,38 +1,34 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id' : 1,
-    'title' : 'Data Analyst',
-    'location' : 'Bengaluru, India',
-    'salary' : 'Rs. 10,00,000'
-  },
-  {
-    'id' : 2,
-    'title' : 'Data Scientist',
-    'location' : 'Delhi, India',
-    'salary' : 'Rs. 15,00,000'
-  },
-  {
-    'id' : 3,
-    'title' : 'Frontend Engineer',
-    'location' : 'Remote',
-    'salary' : 'Rs. 12,00,000'
-  },
-  {
-    'id' : 4,
-    'title' : 'Backend Engineer',
-    'location' : 'San Fransisco, USA',
-    'salary' : '$120,000'
-  }
-]
+JOBS = [{
+  'id': 1,
+  'title': 'Computer Teacher',
+  'location': 'Pimpri, India',
+  'salary': 'Rs. 10,00,000'
+}, {
+  'id': 2,
+  'title': 'Chemistry Teacher',
+  'location': 'Akurdi, India',
+  'salary': 'Rs. 15,00,000'
+}, {
+  'id': 3,
+  'title': 'Therapist',
+  'location': 'Remote',
+  'salary': 'Rs. 12,00,000'
+}, {
+  'id': 4,
+  'title': 'Math Teacher',
+  'location': 'Akurdi, India',
+  'salary': 'Rs. 20,00,000'
+}]
+
 
 @app.route("/")
-
 def mainHTML():
-  return render_template('home.html',jobs = JOBS,title = 'Register For PCCOE')
+  return render_template('home.html', jobs=JOBS, title='Register For PCCOE')
+
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0' , debug = True)
+  app.run(host='0.0.0.0', debug=True)
